d92f4dec8086adb07960f01b7a1a8f664513d987
posadari <57573681+posadari@users.noreply.github.com>
2022-02-26 15:43:54 -0600

    work on home page

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import ResponsiveAppBar from '../components/Navbar'
+import Button from '@mui/material/Button';
+import './Home.css'
+const first = {
+    color: 'black',
+    paddingTop: '2em',
+    textAlign: 'left',
+    position: 'relative',
+    left: '100px',
+    fontSize: '42px',
+    width: '870px',
+    fontFamily: 'Lato',
+    fontWeight: '700'
+}
+
+const second = {
+    color: 'black',
+    textAlign: 'left',
+    position: 'relative',
+    left: '100px',
+    opacity: '0.5',
+    fontFamily: 'Lato',
+    fontWeight: '700'
+}
 
 const Home = () => {
     return (
         <div>
-            Hoem
+            <ResponsiveAppBar />
+            <div style={first}>
+                <h1>Find out if an item is recyclable.</h1>
+            </div>
+            <div style={second}>
+                <h2>Click the button below to begin.</h2>
+            </div>
+            {/* <Button variant="contained" style= {{backgroundColor: "#21b6ae"}}>Contained</Button> */}
         </div>
     )
 }
