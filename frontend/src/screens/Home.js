185db956bf263188f94b9da6f7fa517701cbd322
posadari <57573681+posadari@users.noreply.github.com>
2022-02-26 15:56:01 -0600

    add button

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ResponsiveAppBar from '../components/Navbar'
 import Button from '@mui/material/Button';
 import './Home.css'
+import recycle_icon from '../images/recycle 1.svg'
+
 const first = {
     color: 'black',
     paddingTop: '2em',
@@ -16,12 +18,22 @@ const first = {
 
 const second = {
     color: 'black',
+    paddingBottom: '2em',
     textAlign: 'left',
     position: 'relative',
     left: '100px',
     opacity: '0.5',
     fontFamily: 'Lato',
     fontWeight: '700'
+}
+
+const third = {
+    backgroundColor: "#5F6F3A",
+    position: 'absolute',
+    left: '100px',
+    borderRadius: 15,
+    padding: "9px 36px",
+    fontSize: "18px"
 }
 
 const Home = () => {
@@ -34,7 +46,7 @@ const Home = () => {
             <div style={second}>
                 <h2>Click the button below to begin.</h2>
             </div>
-            {/* <Button variant="contained" style= {{backgroundColor: "#21b6ae"}}>Contained</Button> */}
+            <Button variant="contained" style={third}>START</Button>
         </div>
     )
 }
