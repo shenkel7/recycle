d419087021154df8b01d4a90156f2eeb3fc42f67
Kelly Shen <kellyshenfrog@gmail.com>
2022-02-26 15:12:54 -0600

    tensorflowjs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import "@tensorflow/tfjs";
 import Home from './screens/Home';
 import About from './screens/About';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import { useDispatch } from 'react-redux';
+import PizzaSlice from './store/PizzaSlice';
+
+// TODO change this
+const MODEL_URL = 'model_directory/model.json';
 
 function App() {
-
+  const dispatch = useDispatch();
+  
+  const loadModel = async () => {
+    const model = await loadGraphModel(MODEL_URL);
+    dispatch(PizzaSlice.actions.setModel(model));
+  };
 
   return (
     <div className="App">
