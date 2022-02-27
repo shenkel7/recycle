c533cebfcebf9a3f195e94923aad4e4a67332bd2
Kelly Shen <kellyshenfrog@gmail.com>
2022-02-26 18:38:41 -0600

    image uploading works

--- frontend/src/screens/ImageUpload.tsx
+++ frontend/src/screens/ImageUpload.tsx
@@ -3,15 +3,39 @@ import React from 'react'
 import ResponsiveAppBar from '../components/Navbar';
 import ImageUploading, {ImageListType} from 'react-images-uploading';
 import './ImageUpload.css'
+import { Link, useNavigate } from "react-router-dom";
+import { maxHeight, minHeight } from '@mui/system';
 
 const cardStyle = {
   backgroundColor: "#F2DFAF",
   margin: 10,
   flex: 1,
-  padding: 10,
+  paddingTop: 30,
   justifyContent: 'center',
   alignItems: 'center',
+  minHeight: '50vh',
+  maxHeight: "70vh",
+}
 
+const dragCardStyle = {
+  backgroundColor: "#E5C676",
+  margin: 10,
+  flex: 1,
+  paddingTop: 30,
+  justifyContent: 'center',
+  alignItems: 'center',
+  // height: '50vh'
+  minHeight: '50vh',
+  maxHeight: "70vh",
+
+}
+
+const third = {
+  backgroundColor: "#5F6F3A",
+  borderRadius: 15,
+  padding: "9px 36px",
+  fontSize: "18px",
+  marginTop: 10
 }
 
 const imageStyle = {
@@ -19,9 +43,17 @@ const imageStyle = {
   height: 95,
 }
 
+const textTitle = {
+    fontFamily: 'Lato',
+    fontSize: 30,
+    marginTop: 20,
+    fontWeight: 100
+}
+
 const ImageUpload = () => {
   const [image, setImage] = React.useState([]);
   const maxNumber = 69;
+  let navigate = useNavigate();
 
   const onChange = (
     imageList: ImageListType,
@@ -51,38 +83,54 @@ const ImageUpload = () => {
         }) => (
           // write your building UI
           <div className="upload__image-wrapper">
-            {/* <button
-              style={isDragging ? { color: "red" } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button> */}
             <div style={{display: 'flex', padding: 50}}>
               
               <Card variant="outlined"
               className="card"
-              style={isDragging ? {} : cardStyle}
-              onClick={onImageUpload}
+              style={(image.length !== 0 || isDragging) ? dragCardStyle : cardStyle}
+              onClick={() => {
+                if(image.length === 0) {
+                  onImageUpload() 
+                } else {
+                  onImageUpdate(0)
+                }
+              }}
               {...dragProps}>
-              <img style={imageStyle} src="/assets/file_icon.png"/>
-              </Card>
-
+              {
+                image.length === 0 ? 
+                <div>
+                  <img style={imageStyle} src="/assets/file_icon.png"/>
+                  <div style={textTitle}>Upload an Image</div>
+                  <Button variant="contained" style={third}>Choose File</Button>
+                </div>
+              : 
+                <>
+                  {imageList.map((image, index) => (
+                    <div key={index} className="image-item" style={{height: "100%"}}>
+                      <img src={image.dataURL} alt="" style={{maxHeight: "40vh"}}/>
+                      <div className="image-item__btn-wrapper">
+                        {/* <button onClick={() => onImageUpdate(index)}>Update</button> */}
+                        <Button variant="contained" style={third}>Update</Button>
+  
+                      </div>
+                    </div>
+                  ))}
+                </>
+              }
+              </Card> 
               &nbsp;
               <Card
+              // onMouseEnter={toggleWebcamHover}
               style={cardStyle}
-              onClick={onImageRemoveAll}>
+              onClick={() => {
+                navigate("/webcam");
+              }}
+              >
                 <img style={imageStyle} src="/assets/photo_icon.png" alt="photo_icon" />
+                <div style={textTitle}>Take a Photo</div>
+                <Button variant="contained" style={third}>Open Webcam</Button>
                 </Card>
-              {imageList.map((image, index) => (
-                <div key={index} className="image-item">
-                  <img src={image.dataURL} alt="" width="100" />
-                  <div className="image-item__btn-wrapper">
-                    <button onClick={() => onImageUpdate(index)}>Update</button>
-                    <button onClick={() => onImageRemove(index)}>Remove</button>
-                  </div>
-                </div>
-              ))}
+            
             </div>
           </div>
         )}
